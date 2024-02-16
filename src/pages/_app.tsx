2a43b17e50ebfb44b060bb33faca21894b99776e
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-02-16 11:19:06 -0500

    working dark mode and ligth mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
+import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import { Theme } from "@radix-ui/themes";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SyntaxSchemeProvider } from "../../components/Pre";
+import { Favicon } from "../../components/Favicon";
+import { CssLibPreferenceProvider } from "../../components/CssLibPreference";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function Pages({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  return (
+    <Theme accentColor="indigo" className="radix-themes-custom-fonts">
+      <SyntaxSchemeProvider scheme="indigo">
+        <Favicon />
+        <Component {...pageProps} />
+      </SyntaxSchemeProvider>
+    </Theme>
+  );
 }
+
+function App(props: AppProps) {
+  return (
+    <CssLibPreferenceProvider>
+      <ThemeProvider
+        disableTransitionOnChange
+        attribute="class"
+        value={{ light: "light-theme", dark: "dark-theme" }}
+        defaultTheme="light-theme"
+      >
+        <Pages {...props} />
+      </ThemeProvider>
+    </CssLibPreferenceProvider>
+  );
+}
+
+export default App;
